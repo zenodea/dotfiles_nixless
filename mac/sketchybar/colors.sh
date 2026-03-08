48bcb6ef22b829f35f5914dc1e4c8c5387703329
Zeno de Angeli <zenolino@gmail.com>
2026-03-08 11:56:46 +0000

    feat: updated config files

---
+++ mac/sketchybar/colors.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Color Palette - Nord Theme
+export BLACK=0xff2e3440
+export WHITE=0xffeceff4
+export RED=0xffbf616a
+export GREEN=0xffa3be8c
+export BLUE=0xff88c0d0
+export YELLOW=0xffebcb8b
+export ORANGE=0xffd08770
+export MAGENTA=0xffb48ead
+export GREY=0xff4c566a
+export TRANSPARENT=0x00000000
+
+# General bar colors
+export BAR_COLOR=$TRANSPARENT
+export ICON_COLOR=$WHITE  # Color of all icons
+export LABEL_COLOR=$WHITE # Color of all labels
+export BACKGROUND_1=0x90434c5e
+export BACKGROUND_2=0x904c566a
+export POPUP_BACKGROUND_COLOR=0xff2e3440
+export POPUP_BORDER_COLOR=$WHITE
+export SHADOW_COLOR=$BLACK
